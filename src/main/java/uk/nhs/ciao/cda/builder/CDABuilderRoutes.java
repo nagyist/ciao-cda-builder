e35d07f631ee2a53742c7564252ea4b668fc19c9
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-09-21 11:58:52 +0100

    Added route for in progress directory manager (and associated CIAO property) Moved CDABuilderRoute code into separate class (called from main route builder)

--- src/main/java/uk/nhs/ciao/cda/builder/CDABuilderRoutes.java
+++ src/main/java/uk/nhs/ciao/cda/builder/CDABuilderRoutes.java
@@ -1,15 +1,12 @@
 package uk.nhs.ciao.cda.builder;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
-import org.apache.camel.model.dataformat.JsonLibrary;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
 
-import uk.nhs.ciao.CIPRoutes;
 import uk.nhs.ciao.camel.CamelApplication;
+import uk.nhs.ciao.cda.builder.route.CDABuilderRoute;
 import uk.nhs.ciao.configuration.CIAOConfig;
-import uk.nhs.ciao.exceptions.CIAOConfigurationException;
+import uk.nhs.ciao.docs.parser.route.InProgressFolderManagerRoute;
 
 /**
  * Configures multiple camel CDA builder routes determined by properties specified
@@ -30,100 +27,39 @@ import uk.nhs.ciao.exceptions.CIAOConfigurationException;
  * <dt>outputQueue<dt>
  * <dd>The name of the queue output messages should be sent to</dd>
  */
-public class CDABuilderRoutes extends CIPRoutes {
-	private static final Logger LOGGER = LoggerFactory.getLogger(CDABuilderRoutes.class);
-	
-	/**
-	 * The root property 
-	 */
-	public static final String ROOT_PROPERTY = "cdaBuilderRoutes";
-	
+public class CDABuilderRoutes implements RoutesBuilder {
 	/**
 	 * Creates multiple document parser routes
 	 * 
-	 * @throws RuntimeException If required CIAO-config properties are missing
+	 * @throws Exception If required CIAO-config properties are missing
 	 */
 	@Override
-	public void configure() {
-		super.configure();
+	public void addRoutesToCamelContext(final CamelContext context) throws Exception {
+		addCDABuilderRoute(context);
 		
-		final CIAOConfig config = CamelApplication.getConfig(getContext());
+		// Services
+		addInProgressFolderManagerRoute(context);
+	}
+	
+	private void addCDABuilderRoute(final CamelContext context) throws Exception {
+		final CIAOConfig config = CamelApplication.getConfig(context);
 		
-		try {
-			final String[] routeNames = config.getConfigValue(ROOT_PROPERTY).split(",");
-			for (final String routeName: routeNames) {
-				final CDABuilderRouteBuilder builder = new CDABuilderRouteBuilder(
-						routeName, config);
-				builder.configure();
-			}
-		} catch (CIAOConfigurationException e) {
-			throw new RuntimeException("Unable to build routes from CIAOConfig", e);
+		final String[] routeNames = config.getConfigValue(CDABuilderRoute.ROOT_PROPERTY).split(",");
+		for (final String routeName: routeNames) {
+			final CDABuilderRoute route = new CDABuilderRoute(
+					routeName, config);
+			route.setInProgressFolderManagerUri("direct:in-progress-folder-manager");
+			context.addRoutes(route);
 		}
 	}
 	
-	/**
-	 * Creates a Camel route for the specified name / property prefix.
-	 * <p>
-	 * Each configurable property is determined by:
-	 * <ul>
-	 * <li>Try the specific property: <code>${ROOT_PROPERTY}.${name}.${propertyName}</code></li>
-	 * <li>If missing fallback to: <code>${ROOT_PROPERTY}.${propertyName}</code></li>
-	 * </ul>
-	 */
-	private class CDABuilderRouteBuilder {
-		private final String name;
-		private final String inputQueue;
-		private final String processorId;
-		private final String outputQueue;
+	private void addInProgressFolderManagerRoute(final CamelContext context) throws Exception {
+		final InProgressFolderManagerRoute route = new InProgressFolderManagerRoute();
 		
-		/**
-		 * Creates a new route builder for the specified name / property prefix
-		 * 
-		 * @param name The route name / property prefix
-		 * @throws CIAOConfigurationException If required properties were missing
-		 */
-		public CDABuilderRouteBuilder(final String name, final CIAOConfig config) throws CIAOConfigurationException {
-			this.name = name;
-			this.inputQueue = findProperty(config, "inputQueue");
-			this.processorId = findProperty(config, "processorId");
-			this.outputQueue = findProperty(config, "outputQueue");
-		}
+		route.setInProgressFolderManagerUri("direct:in-progress-folder-manager");
+		route.setInternalRoutePrefix("in-progress-folder-manager");
+		route.setInProgressFolderRootUri("file:{{inProgressFolder}}");
 		
-		/**
-		 * Try the specific 'named' property then fall back to the general 'all-routes' property
-		 */
-		private String findProperty(final CIAOConfig config, final String propertyName) throws CIAOConfigurationException {
-			final String specificName = ROOT_PROPERTY + "." + name + "." + propertyName;
-			final String genericName = ROOT_PROPERTY + "." + propertyName;
-			if (config.getConfigKeys().contains(specificName)) {
-				return config.getConfigValue(specificName);
-			} else if (config.getConfigKeys().contains(genericName)) {
-				
-				return config.getConfigValue(genericName);
-			} else {
-				throw new CIAOConfigurationException("Could not find property " + propertyName +
-						" for route " + name);
-			}
-		}
-
-		/**
-		 * Configures / creates a new Camel route corresponding to the set of CIAO-config
-		 * properties associated with the route name.
-		 */
-		@SuppressWarnings("deprecation")
-		public void configure() {
-			from("jms:queue:" + inputQueue)
-			.id("cda-builder-" + name)
-			.doTry()
-				.log(LoggingLevel.INFO, LOGGER, "Unmarshalled incoming JSON document")
-				.beanRef(processorId, "transform")
-				.marshal().json(JsonLibrary.Jackson)
-				.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}.json"))
-				.to("jms:queue:" + outputQueue)
-			.doCatch(Exception.class)
-				.log(LoggingLevel.ERROR, LOGGER, "Exception while builder CDA document")
-				.to("log:" + LOGGER.getName() + "?level=ERROR&showCaughtException=true")
-				.handled(false);
-		}
+		context.addRoutes(route);
 	}
 }
---
+++ src/main/java/uk/nhs/ciao/cda/builder/route/CDABuilderRoute.java
@@ -0,0 +1,90 @@
+package uk.nhs.ciao.cda.builder.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.nhs.ciao.camel.BaseRouteBuilder;
+import uk.nhs.ciao.configuration.CIAOConfig;
+import uk.nhs.ciao.exceptions.CIAOConfigurationException;
+
+/**
+ * Creates a Camel route for the specified name / property prefix.
+ * <p>
+ * Each configurable property is determined by:
+ * <ul>
+ * <li>Try the specific property: <code>${ROOT_PROPERTY}.${name}.${propertyName}</code></li>
+ * <li>If missing fallback to: <code>${ROOT_PROPERTY}.${propertyName}</code></li>
+ * </ul>
+ */
+public class CDABuilderRoute extends BaseRouteBuilder {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CDABuilderRoute.class);
+	
+	/**
+	 * The root property 
+	 */
+	public static final String ROOT_PROPERTY = "cdaBuilderRoutes";
+	
+	private final String name;
+	private final String inputQueue;
+	private final String processorId;
+	private final String outputQueue;
+	private String inProgressFolderManagerUri;
+	
+	/**
+	 * Creates a new route builder for the specified name / property prefix
+	 * 
+	 * @param name The route name / property prefix
+	 * @throws CIAOConfigurationException If required properties were missing
+	 */
+	public CDABuilderRoute(final String name, final CIAOConfig config) throws CIAOConfigurationException {
+		this.name = name;
+		this.inputQueue = findProperty(config, "inputQueue");
+		this.processorId = findProperty(config, "processorId");
+		this.outputQueue = findProperty(config, "outputQueue");
+	}
+	
+	public void setInProgressFolderManagerUri(final String inProgressFolderManagerUri) {
+		this.inProgressFolderManagerUri = inProgressFolderManagerUri;
+	}
+	
+	/**
+	 * Try the specific 'named' property then fall back to the general 'all-routes' property
+	 */
+	private String findProperty(final CIAOConfig config, final String propertyName) throws CIAOConfigurationException {
+		final String specificName = ROOT_PROPERTY + "." + name + "." + propertyName;
+		final String genericName = ROOT_PROPERTY + "." + propertyName;
+		if (config.getConfigKeys().contains(specificName)) {
+			return config.getConfigValue(specificName);
+		} else if (config.getConfigKeys().contains(genericName)) {
+			
+			return config.getConfigValue(genericName);
+		} else {
+			throw new CIAOConfigurationException("Could not find property " + propertyName +
+					" for route " + name);
+		}
+	}
+
+	/**
+	 * Configures / creates a new Camel route corresponding to the set of CIAO-config
+	 * properties associated with the route name.
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	public void configure() throws Exception {
+		from("jms:queue:" + inputQueue)
+		.id("cda-builder-" + name)
+		.doTry()
+			.log(LoggingLevel.INFO, LOGGER, "Unmarshalled incoming JSON document")
+			.beanRef(processorId, "transform")
+			.marshal().json(JsonLibrary.Jackson)
+			.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}.json"))
+			.to("jms:queue:" + outputQueue)
+		.doCatch(Exception.class)
+			.log(LoggingLevel.ERROR, LOGGER, "Exception while builder CDA document")
+			.to("log:" + LOGGER.getName() + "?level=ERROR&showCaughtException=true")
+			.handled(false);
+	}
+}
